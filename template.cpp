4ae402a2fdc8164ba0565c431a2c161035278fec
Koki Mizumoto <21km43@gmail.com>
2026-07-18 16:49:57 +0900

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    
+
+    cout << "answer" << endl;
+    return 0;
+}
