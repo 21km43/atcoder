5e339609f3d615eea8b4bff73c4ec4e70daa9352
Koki Mizumoto <21km43@gmail.com>
2026-08-23 21:12:35 +0900

    fix template.cpp

--- template.cpp
+++ template.cpp
@@ -1,19 +1,18 @@
-#include <bits/stdc++.h>
 #include <atcoder/all>
+#include <bits/stdc++.h>
 using namespace std;
 using namespace atcoder;
 using ll = long long;
 using ull = unsigned long long;
 
 int main() {
-    ll N;
-    cin >> N;
-    vector<ll> A(N);
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
-    }
-    
+  ll N;
+  cin >> N;
+  vector<ll> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
 
-    cout << "answer" << endl;
-    return 0;
+  cout << "answer" << endl;
+  return 0;
 }
